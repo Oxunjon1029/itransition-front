ec717d95fdbd670340f63841419e72d639024b3e
Oxunjon1029 <abdusalomov2019oxunjon@gmail.com>
2023-05-27 22:44:29 +0500

    change

--- src/config/constants/host.js
+++ src/config/constants/host.js
@@ -1,5 +1,5 @@
 import { getCookie } from '../functions/useCookie';
-export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'http://192.168.1.5:5000';
 export const TOKEN = 'ITRANSITION_TOKEN';
 const token = getCookie(TOKEN);
 export const headers = {
